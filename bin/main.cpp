f5305c1236fb365079a3d64ee7575911364d92e9
DaryaPanyukova <panyukova.darya@bk.ru>
2022-10-14 17:50:02 +0300

    Revert "vector replaced with an array"
    This reverts commit 7e5317e8

--- bin/main.cpp
+++ bin/main.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 int main() {
-    uint2022_t a = from_string("200000000234567899087654678900000000000012");
-    uint2022_t b = from_string("1197865411111111");
+    uint2022_t a = from_string("999999999999999999");
+    uint2022_t b = from_string("111111111111111111");
 
     uint2022_t c = a / b;
     std::cout << c;
